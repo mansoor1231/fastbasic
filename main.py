74049b218688a32fdb526b51d2030d360b842bfe
mansoor1231 <105422855+mansoor1231@users.noreply.github.com>
2024-03-13 22:54:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,107 @@
-from typing import List
-import databases
-import sqlalchemy
-from fastapi import FastAPI, status
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
-from pydantic import BaseModel
-import os
-import urllib
+from fastapi import  FastAPI
+import psycopg2
+import re
+print("Connecting")
 
-#DATABASE_URL = "sqlite:///./test.db"
+# conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+#                             password="brocafe", port=5432)
+# conn = psycopg2.connect(host=host_name, dbname=database_name, user=user_name,
+#                             password=user_pass, port=port_name)
+conn = psycopg2.connect(host="fastapi-server.postgres.database.azure.com", dbname="postgres", user="plantfastapi",
+                            password="IOTmonitor1!", port=5432)
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS soil_monitor (
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL DEFAULT CURRENT_DATE,
+    time TIME NOT NULL DEFAULT CURRENT_TIME,
+    DeviceName VARCHAR(255),
+    LightIntensity FLOAT,
+    SoilMoisture FLOAT,
+    Humidity FLOAT,
+    Temperature FLOAT);""")
+conn.commit()
+print("Connected")
+app = FastAPI()
 
-host_server = os.environ.get('host_server', 'localhost')
-db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'fastapi')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
-ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
-DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
+@app.get("/")
+async def root():
+    return {"Plant": "hello world"}
+@app.post("/")
+async def post():
+    return {"message": "hello post"}
+@app.get("/get")
+async def get_info():
+    # cur = conn.cursor()
+    cur.execute("""SELECT *
+        FROM soil_monitor
+        ORDER BY id DESC
+        LIMIT 1;""")
+    conn.commit()
+    result = cur.fetchone()
+    print(result)
 
-database = databases.Database(DATABASE_URL)
+    int_value = int(result[0])
+    date = result[1]
+    time = result[2]
+    device_name = result[3]
+    light = int(result[4])
+    soil = int(result[5])
+    humid = int(result[6])
+    temp = int(result[7])
+    # int_value = 25
+    # date = "2024/03/19"
+    # time = "13:10:44"
+    # device_name = "Pi"
+    # light = 3.54
+    # soil = 5.48
+    # humid = 60.4
+    # temp = 32.45
+    # Print the extracted values
+    print("Integer:", int_value)
+    print("Time:", time)
+    print("Date:", date)
+    print("Device Name:", device_name)
+    print("Lght Intensity:%fcd" % light)
+    print("Soil Moisture:%fm~/m3" % soil)
+    print("Humidity:%f%%" % humid)
+    print("Temperature:%f°C" % temp)
+    return {"Date":date,"Time":time,"Device Name":device_name,
+            "Lght Intensity":light,"Soil Moisture":soil,"Humidity":humid,"Temperature":temp
+            }
 
-metadata = sqlalchemy.MetaData()
+@app.get("/post/{dname}")
+async def post_data(dname: str):
+    #                name,  light, soil,humid, temp
+    # data_string = "device_25.23_45.68_125.25_23.751"
 
-notes = sqlalchemy.Table(
-    "notes",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
-)
-
-engine = sqlalchemy.create_engine(
-    #DATABASE_URL, connect_args={"check_same_thread": False}
-    DATABASE_URL, pool_size=3, max_overflow=0
-)
-metadata.create_all(engine)
-
-class NoteIn(BaseModel):
-    text: str
-    completed: bool
-
-class Note(BaseModel):
-    id: int
-    text: str
-    completed: bool
-
-app = FastAPI(title="REST API using FastAPI PostgreSQL Async EndPoints")
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-app.add_middleware(GZipMiddleware)
-
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
-
-@app.post("/notes/", response_model=Note, status_code = status.HTTP_201_CREATED)
-async def create_note(note: NoteIn):
-    query = notes.insert().values(text=note.text, completed=note.completed)
-    last_record_id = await database.execute(query)
-    return {**note.dict(), "id": last_record_id}
-
-@app.put("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
-async def update_note(note_id: int, payload: NoteIn):
-    query = notes.update().where(notes.c.id == note_id).values(text=payload.text, completed=payload.completed)
-    await database.execute(query)
-    return {**payload.dict(), "id": note_id}
-
-@app.get("/notes/", response_model=List[Note], status_code = status.HTTP_200_OK)
-async def read_notes(skip: int = 0, take: int = 20):
-    query = notes.select().offset(skip).limit(take)
-    return await database.fetch_all(query)
-
-@app.get("/notes/{note_id}/", response_model=Note, status_code = status.HTTP_200_OK)
-async def read_notes(note_id: int):
-    query = notes.select().where(notes.c.id == note_id)
-    return await database.fetch_one(query)
-
-@app.delete("/notes/{note_id}/", status_code = status.HTTP_200_OK)
-async def delete_note(note_id: int):
-    query = notes.delete().where(notes.c.id == note_id)
-    await database.execute(query)
-    return {"message": "Note with id: {} deleted successfully!".format(note_id)}
+    pattern = r'([^_]+)_([-+]?[0-9]*\.?[0-9]+)_([-+]?[0-9]*\.?[0-9]+)_([-+]?[0-9]*\.?[0-9]+)_([-+]?[0-9]*\.?[0-9]+)'
+    match = re.match(pattern, dname)
+    if match:
+        device = match.group(1)
+        light = float(match.group(2))
+        soil = float(match.group(3))
+        humid = float(match.group(4))
+        temp = float(match.group(5))
+        # Print the extracted components
+        print("Device:", device)
+        print("Float 1:", light)
+        print("Float 2:", soil)
+        print("Float 3:", humid)
+        print("Float 4:", temp)
+    else:
+        print("No match found.")
+        device = ""
+        light = 0
+        soil = 0
+        humid = 0
+        temp = 0
+    # conn = psycopg2.connect(host="localhost", dbname="fastapi", user="postgres",
+    #                         password="brocafe", port=5432)
+    # cur = conn.cursor()
+    cur.execute("""INSERT INTO soil_monitor (DeviceName, LightIntensity, SoilMoisture, Humidity, Temperature) VALUES
+    ('"""+device+"""', %f, %f, %f,%f);
+    """ % ( light, soil , humid, temp))
+    conn.commit()
+    # cur.close()
+    # conn.close()
+    return {"#Data Posted":dname}
